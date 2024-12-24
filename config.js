a01df761ff8a42f252f96d6a8c9dc9a5586dd46d
Akan <jsuispashumain2@gmail.com>
2024-12-24 22:17:22 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: '',
+        owners: ["245573481041756161"],
+        funny: '245573481041756161',
+        color: '#2f3136',
+        footer: 'Akan Bot',
+        maxserver: '100',
+        maxVol: '150',
+        everyoneMention: true,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
+// merci de laisser les crédits, le bot m'a pris du temps à faire
